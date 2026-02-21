d83da275cd2d9a7e82028f80e139ba4390e30070
amarpal <sandhuamar07@gmail.com>
2026-02-21 22:06:10 +0530

    feat: frontend init

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,20 +1,5 @@
-import { Routes, Route } from 'react-router-dom'
-import Dashboard from './pages/Dashboard'
-import CreateMatch from './pages/CreateMatch'
-import AccessCode from './pages/AccessCode'
-import Scoring from './pages/Scoring'
-
 function App() {
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/create-match" element={<CreateMatch />} />
-        <Route path="/access/:matchId" element={<AccessCode />} />
-        <Route path="/scoring/:matchId" element={<Scoring />} />
-      </Routes>
-    </div>
-  )
+  return <div className="min-h-screen bg-green-500 text-red-500">Scorer</div>;
 }
 
-export default App
+export default App;
